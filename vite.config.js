cb1161a12a343b63021b5bb8f50fdb95be20b3fb
sonpt26 sonpt26 <secret.victoria.noob@gmail.com>
2025-04-09 10:39:23 +0000

    add allowedhost

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   plugins: [react(), cloudflare()],
+  server: {
+    allowedHosts: ["5173-vandertyson-restlesstre-xnr399i6hhz.ws-us118.gitpod.io"]
+  }
 });
