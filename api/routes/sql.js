cf2887b54793c0a443ca1f6537e7f9fda9183216
sonpt26 sonpt26 <secret.victoria.noob@gmail.com>
2025-04-15 13:34:50 +0000

    log bugs

--- api/routes/sql.js
+++ api/routes/sql.js
@@ -2,22 +2,34 @@ import { Hono } from 'hono';
 
 const sqlExecutor = new Hono();
 
-sqlExecutor.post('/query', async (c) => {  
-  const {query} = await c.req.json();
+sqlExecutor.post('/query', async (c) => {
+  try {
+    const {query} = await c.req.json();
 
-  const {results} = await c.env.DB.prepare(query).all()
-  console.log("run query", query, results) 
+    const {results} = await c.env.DB.prepare(query).all()
+    console.log("run query", query, results) 
 
-  return Response.json(results);
+    return Response.json(results);  
+  } catch (error) {
+    console.log("Error query sql")
+    console.trace(error)
+    throw error
+  }  
 });
 
 sqlExecutor.post('/mutate', async (c) => { 
+  try {
     const {query} = await c.req.json();
   
     const info = await c.env.DB.prepare(query).run()
     console.log("run mutate", query, info) 
   
-    return Response.json(info);
-  });
+    return Response.json(info);    
+  } catch (error) {
+    console.log("Error mutate sql")
+    console.trace(error)
+    throw error
+  }
+});
 
 export default sqlExecutor;
